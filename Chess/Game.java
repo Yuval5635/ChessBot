dde6af0b4125fa8d1292b326cae72c81771acbc0
Yuval5635 <yuyudiamant@gmail.com>
2026-06-09 01:13:06 +0300

    Adding isWhiteTurn property and resetGame function andisWin function to Game

--- Chess/Game.java
+++ Chess/Game.java
@@ -2,13 +2,37 @@ public class Game {
 
     private static Game instance;
 
-    private Board board = Board.getInstance();
+    private Board board;
+    private boolean isWhiteTurn;
 
     public static Game getInstance() {
         if (instance == null) instance = new Game();
         return instance;
     }
 
-    private Game() {}
+    private Game() {
+        this.board = Board.getInstance();
+        this.isWhiteTurn = true;
+    }
+
+    public void resetGame(){
+        instance = new Game();
+    }
+
+    public int isWin() {
+        boolean whiteKingAlive = false;
+        boolean blackKingAlive = false;
+
+        for (int i = 0; i < 64; i++){
+            if (board.isOccupy(i) && board.getSquare(i) instanceof King){
+                if (board.getColor(i) == 1) whiteKingAlive = true;
+                else blackKingAlive = true;
+            }
+        }
+
+        if (!whiteKingAlive) return -1;
+        else if (!blackKingAlive) return 1;
+        else return 0;
+    }
 
 }
