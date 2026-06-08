7ace63522d8127c52f97166d5758e28a2fea98ab
Yuval5635 <yuyudiamant@gmail.com>
2026-06-08 23:56:49 +0300

    adding the logic for getMoves function to Rook

--- Chess/Rook.java
+++ Chess/Rook.java
@@ -4,4 +4,43 @@ public class Rook extends Piece{
         super(color, square);
     }
 
+    @Override
+    public int[] getMoves(){
+        boolean[] isMoves = new boolean[64];
+
+        for (int i = -1; i < 2; i += 2){
+            for (int j = i; true; j += i){
+                if (isValidRow(j)){
+                    isMoves[(j * 8) + this.square] = true;
+                } else{
+                    break;
+                }
+            }
+            for (int j = i; true; j += i){
+                if (isValidCol(j)){
+                    isMoves[j + this.square] = true;
+                } else{
+                    break;
+                }
+            }
+        }
+
+        int numOfValidMoves = 0;
+        for (boolean isMove : isMoves) {
+            if (isMove) numOfValidMoves++;
+        }
+
+        int[] validMoves = new int[numOfValidMoves];
+        return validMoves;
+    }
+
+    private boolean isValidRow(int rowOffset){
+        int row = rowOffset + (this.square / 8);
+        return row < 8 && row >= 0 && (! Board.getInstance().isOccupy(row + this.square % 8));
+    }
+
+    private boolean isValidCol(int colOffset){
+        int col = colOffset + (this.square % 8);
+        return col < 8 && col >= 0 && (! Board.getInstance().isOccupy(col + this.square / 8));
+    }
 }
