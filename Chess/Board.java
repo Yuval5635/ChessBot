e2dd374be22aed1f29f85ebb1251fb4367f0dc4f
Yuval5635 <yuyudiamant@gmail.com>
2026-06-08 23:57:33 +0300

    Board is Singleton and have the isOccupy function

--- Chess/Board.java
+++ Chess/Board.java
@@ -1,6 +1,21 @@
-public class Board{
 
-    public Board(){
+public class Board {
+
+    private static Board instance;
+
+    private Piece[] board;
+
+    public static Board getInstance() {
+        if (instance == null) instance = new Board();
+        return instance;
     }
-    
+
+    private Board() {
+        this.board = new Piece[]{new Rook(1, 0), new Knight(1, 1)};
+    }
+
+    public boolean isOccupy(int square){
+        return board[square] == null;
+    }
+
 }
