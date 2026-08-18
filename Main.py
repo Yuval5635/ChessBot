9d2cc09fee623e2f4424d87007d66e40d1514dbf
Yuval5635 <yuyudiamant@gmail.com>
2026-08-18 13:21:58 +0300

    the python side is working

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import subprocess
 import shutil
 from py4j.java_gateway import JavaGateway, GatewayParameters, launch_gateway
 import pygame
+import threading
 
 BASE = os.path.dirname(os.path.abspath(__file__))
 
@@ -40,31 +41,33 @@ main = gateway.jvm.chess.Main()
 def add_piece(win, piece, x, y):
     piece_type = piece.getName()
 
+    cords = (x * 100, (7 - y) * 100)
+
     if piece_type == "p":
-        win.blit(black_pawn_img, (x * 100, y * 100))
+        win.blit(black_pawn_img, cords)
     elif piece_type == "b":
-        win.blit(black_bishop_img, (x * 100, y * 100))
+        win.blit(black_bishop_img, cords)
     elif piece_type == "n":
-        win.blit(black_knight_img, (x * 100, y * 100))
+        win.blit(black_knight_img, cords)
     elif piece_type == "r":
-        win.blit(black_rook_img, (x * 100, y * 100))
+        win.blit(black_rook_img, cords)
     elif piece_type == "q":
-        win.blit(black_queen_img, (x * 100, y * 100))
+        win.blit(black_queen_img, cords)
     elif piece_type == "k":
-        win.blit(black_king_img, (x * 100, y * 100))
+        win.blit(black_king_img, cords)
 
     elif piece_type == "P":
-        win.blit(white_pawn_img, (x * 100, y * 100))
+        win.blit(white_pawn_img, cords)
     elif piece_type == "B":
-        win.blit(white_bishop_img, (x * 100, y * 100))
+        win.blit(white_bishop_img, cords)
     elif piece_type == "N":
-        win.blit(white_knight_img, (x * 100, y * 100))
+        win.blit(white_knight_img, cords)
     elif piece_type == "R":
-        win.blit(white_rook_img, (x * 100, y * 100))
+        win.blit(white_rook_img, cords)
     elif piece_type == "Q":
-        win.blit(white_queen_img, (x * 100, y * 100))
+        win.blit(white_queen_img, cords)
     elif piece_type == "K":
-        win.blit(white_king_img, (x * 100, y * 100))
+        win.blit(white_king_img, cords)
 
 def draw_board(surface):
     """Draws the alternating chess board pattern."""
@@ -85,19 +88,40 @@ def draw_board(surface):
 
 screen = pygame.display.set_mode((800, 800))
 
-black_pawn_img = pygame.image.load("images/BlackPawn.png").convert_alpha()
-black_bishop_img = pygame.image.load("images/BlackBishop.png").convert_alpha()
-black_knight_img = pygame.image.load("images/BlackKnight.png").convert_alpha()
-black_rook_img = pygame.image.load("images/BlackRook.png").convert_alpha()
-black_queen_img = pygame.image.load("images/BlackQueen.png").convert_alpha()
-black_king_img = pygame.image.load("images/BlackKing.png").convert_alpha()
+black_pawn_img = pygame.transform.scale(pygame.image.load("images/BlackPawn.png").convert_alpha(), (100, 100))
+black_bishop_img = pygame.transform.scale(pygame.image.load("images/BlackBishop.png").convert_alpha(), (100, 100))
+black_knight_img = pygame.transform.scale(pygame.image.load("images/BlackKnight.png").convert_alpha(), (100, 100))
+black_rook_img = pygame.transform.scale(pygame.image.load("images/BlackRook.png").convert_alpha(), (100, 100))
+black_queen_img = pygame.transform.scale(pygame.image.load("images/BlackQueen.png").convert_alpha(), (100, 100))
+black_king_img = pygame.transform.scale(pygame.image.load("images/BlackKing.png").convert_alpha(), (100, 100))
 
-white_pawn_img = pygame.image.load("images/WhitePawn.png").convert_alpha()
-white_bishop_img = pygame.image.load("images/WhiteBishop.png").convert_alpha()
-white_knight_img = pygame.image.load("images/WhiteKnight.png").convert_alpha()
-white_rook_img = pygame.image.load("images/WhiteRook.png").convert_alpha()
-white_queen_img = pygame.image.load("images/WhiteQueen.png").convert_alpha()
-white_king_img = pygame.image.load("images/WhiteKing.png").convert_alpha()
+white_pawn_img = pygame.transform.scale(pygame.image.load("images/WhitePawn.png").convert_alpha(), (100, 100))
+white_bishop_img = pygame.transform.scale(pygame.image.load("images/WhiteBishop.png").convert_alpha(), (100, 100))
+white_knight_img = pygame.transform.scale(pygame.image.load("images/WhiteKnight.png").convert_alpha(), (100, 100))
+white_rook_img = pygame.transform.scale(pygame.image.load("images/WhiteRook.png").convert_alpha(), (100, 100))
+white_queen_img = pygame.transform.scale(pygame.image.load("images/WhiteQueen.png").convert_alpha(), (100, 100))
+white_king_img = pygame.transform.scale(pygame.image.load("images/WhiteKing.png").convert_alpha(), (100, 100))
+
+def terminal_thread():
+    global terminal_input
+    while running:
+        text = input("Enter command: ")
+        if text.isdigit() and len(text) == 4:
+            terminal_input = text
+
+
+running = True
+terminal_input = None
+turn = "Player"
+
+threading.Thread(target=terminal_thread, daemon=True).start()
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("Pygame + Terminal")
+
+clock = pygame.time.Clock()
 
 while True:
 
@@ -106,7 +130,6 @@ while True:
             pygame.quit()
             exit()
 
-    main.update()
     board = main.getBoard()
 
     draw_board(screen)    
@@ -118,4 +141,18 @@ while True:
                 add_piece(screen, piece, x, y)
 
     pygame.display.update()
-    
+
+    if turn == "Player":
+        if terminal_input is not None:
+            if main.isMoveValid(int(terminal_input[0:2]), int(terminal_input[2:4])):
+                main.turn(int(terminal_input[0:2]), int(terminal_input[2:4]))
+                turn = "Bot"
+                continue
+            terminal_input = None
+            
+
+    if turn == "Bot":
+        main.botTurn()
+        turn = "Player"
+
+    clock.tick(60)
